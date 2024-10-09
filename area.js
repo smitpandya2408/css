e0bcae9056326aad9a0f70788fba5d0c4942ca39
smitpandya3873 <155702858+smitpandya3873@users.noreply.github.com>
2024-10-09 15:43:08 +0530

    potion

---
+++ area.js
@@ -0,0 +1,26 @@
+//
+// a = 10;
+// b = 20;
+// c = 5;
+
+// sum = (a * b) / c + (b - a);
+// console.log(sum);
+
+// x = 5;
+// y = 10;
+
+// x += 2;
+
+// console.log(x * y);
+
+// num = 10;
+// num += 5;
+// num -= 5;
+// num *= 3;
+// num /= 2;
+// console.log(num);
+
+// score1 = "apple";
+// score2 = "banana";
+
+// console.log(score1 == score2);
